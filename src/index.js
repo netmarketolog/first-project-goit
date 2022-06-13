1895e84e459594fd3882704c3b99e11c96533d13
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2022-06-14 01:10:58 +0300

    add new modal

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector(".header-menu__open"),
-    closeMenuBtn: document.querySelector(".mob-menu__close"),
-    menu: document.querySelector(".mob-menu"),
-    body: document.querySelector('body')
-  };
+    const refs = {
+        openMenuBtn: document.querySelector(".header-menu__open"),
+        closeMenuBtn: document.querySelector(".mob-menu__close"),
+        menu: document.querySelector(".mob-menu"),
+        body: document.querySelector('body')
+    };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+    refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle('no-scroll');
-  }
-})(); 
+    function toggleMenu() {
+        refs.menu.classList.toggle("is-hidden");
+        refs.body.classList.toggle('no-scroll');
+    }
+})();
+
+////////// скрипт до модалки //////////
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector(".tab-button__order"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
