e749bb8560dc63b1f49c9c688fdd2b3c0dfa3b25
netmarketolog <99245672+netmarketolog@users.noreply.github.com>
2022-06-13 15:40:27 +0000

    Deploying to gh-pages from @ netmarketolog/first-project-goit@8839392d20c5fb99978f6b4b903dbe36b5c07ec9 🚀

---
+++ index.d3f7b770.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("body")};function e(){o.body.classList.toggle("no-scroll"),o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.d3f7b770.js.map
