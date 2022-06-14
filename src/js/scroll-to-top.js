535088453ee4e4a9e334629c8a187e3f3c8bb853
Maksym Dorohokuplya <dorogmaks@gmail.com>
2022-06-14 19:38:51 +0300

    feacher scroll to top
    добавил кнопу скролла вверх

---
+++ src/js/scroll-to-top.js
@@ -0,0 +1,36 @@
+const offset = 100;
+const scrollUp = document.querySelector('.scroll-up');
+const scrollUpSvgPath = document.querySelector('.scroll-up__sircle-path');
+const pathLength = scrollUpSvgPath.getTotalLength();
+
+scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
+
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+// updateDashoffset
+const updateDashoffset = () => {
+  const height = document.documentElement.scrollHeight - window.innerHeight;
+  const dashoffset = pathLength - (getTop() * pathLength) / height;
+
+  scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+};
+
+// onScroll
+window.addEventListener('scroll', () => {
+  updateDashoffset();
+
+  if (getTop() > offset) {
+    scrollUp.classList.add('scroll-up--active');
+  } else {
+    scrollUp.classList.remove('scroll-up--active');
+  }
+});
+
+// click
+scrollUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
