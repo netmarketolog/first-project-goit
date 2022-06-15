9b6a4afcc2d7112ebcff734b0fe40e329ba139b6
netmarketolog <99245672+netmarketolog@users.noreply.github.com>
2022-06-15 13:05:33 +0000

    Deploying to gh-pages from @ netmarketolog/first-project-goit@d0d0210547e1c13e1852f53bd1bfffbcb00b8974 🚀

---
+++ index.af5e5bee.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.af5e5bee.js.map
--- index.e9511c23.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.e9511c23.js.map
