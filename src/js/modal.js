33dbc276cb77575d0360eedda31e99fede4488a1
Vetali1985 <geelycross1985@gmail.com>
2022-09-30 20:52:01 +0300

    fix modal-map

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,16 +1,28 @@
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector(".btn-about"),
-        closeModalBtn: document.querySelector(".modal-close"),
-        modal: document.querySelector(".backdrop"),
-        body: document.querySelector('body')
-    };
+  const refs = {
+    openModalBtn: document.querySelector('.btn-about'),
+    closeModalBtn: document.querySelector('.modal-close'),
+    modal: document.querySelector('.backdrop'),
+    body: document.querySelector('body'),
+    map: document.querySelector('.contacts__button--locations'),
+    btnMap: document.querySelector('.btn-modal'),
+    modalMap: document.querySelector('.backdrop-map'),
+  };
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+  console.log(refs.map);
+  console.log(refs.btnMap);
 
-    function toggleModal() {
-        document.body.classList.toggle("no-scroll");
-        refs.modal.classList.toggle("is-hidden");
-    }
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.map.addEventListener('click', map);
+  refs.btnMap.addEventListener('click', map);
+
+  function toggleModal() {
+    document.body.classList.toggle('no-scroll');
+    refs.modal.classList.toggle('is-hidden');
+  }
+  function map() {
+    document.body.classList.toggle('no-scroll');
+    refs.modalMap.classList.toggle('is-hidden');
+  }
 })();
